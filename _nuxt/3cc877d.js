871d56608614d452311d87df5e818f7121c51db5
Ignacio Salas R <146854109+Nacho-SR@users.noreply.github.com>
2024-11-09 11:19:11 -0600

    Updates

---
+++ _nuxt/3cc877d.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[4],{590:function(t,n,o){"use strict";o.r(n);var l={layout:"dashboard",auth:!0},r=o(31),component=Object(r.a)(l,(function(){return(0,this._self._c)("div",[this._v("Laboratorio")])}),[],!1,null,null,null);n.default=component.exports}}]);
